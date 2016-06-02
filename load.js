e9e5fd9480b0460afae90d99fa2b1609d68774f6
Jared Neems <jared.neems@cityrdbclab5017.auckland.natcoll.ac.nz>
2016-06-02 18:16:38 +1200

    new

---
+++ load.js
@@ -0,0 +1,14 @@
+// NOTE: This example will not work locally in all browsers.
+// In Chrome, if you try this locally, you may get an error along the lines of:
+
+
+$('nav a').on('click', function(e) {                 // User clicks nav link
+  e.preventDefault();                                // Stop loading new link
+  var url = this.href;                               // Get value of href
+
+  $('nav a.current').removeClass('current');         // Clear current indicator
+  $(this).addClass('current');                       // New current indicator
+
+  $('#container').remove();                          // Remove old content
+  $('#content').load(url + ' #container').hide().fadeIn('slow'); // New content
+});
